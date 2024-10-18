08aeb32aa31d114cfcf492792ca2e5dd677d6837
SJ0826 <ikosdu60@gmail.com>
2024-10-18 14:20:28 +0900

    project init

---
+++ src/main/java/com/example/scrap_chef/ScrapChefApplication.java
@@ -0,0 +1,13 @@
+package com.example.scrap_chef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapChefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapChefApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scrap_chef/ScrapChefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scrap_chef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapChefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
