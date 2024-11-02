520a1332f719000bcbe717ad589cf9568baa0bbf
SJ0826 <ikosdu60@gmail.com>
2024-11-02 16:41:29 +0900

    feat: localhost CORS 설정

--- src/main/java/com/example/scrap_chef/config/WebClientConfig.java
+++ src/main/java/com/example/scrap_chef/config/WebClientConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
-public class WebClientConfig {
+public class WebClientConfig implements WebMvcConfigurer {
 
     @Bean
     public DefaultUriBuilderFactory builderFactory() {
@@ -23,6 +25,13 @@ public class WebClientConfig {
                 .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .uriBuilderFactory(builderFactory())
                 .build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 
 }
